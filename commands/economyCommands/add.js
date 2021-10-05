37b2649e62c30a90363ed77ff3f03dd760021b65
Eamon <eamon.wong@outlook.com>
2021-10-06 03:27:50 +0800

    Revert "Update add.js"
    This reverts commit 02e2c1c39f6e167e95c3145eacdb41ea6641dfa7.

--- commands/economyCommands/add.js
+++ commands/economyCommands/add.js
@@ -4,7 +4,6 @@
 /* eslint-disable no-await-in-loop */
 const Discord = require('discord.js');
 module.exports = {
-<<<<<<< Updated upstream
   name: 'add',
   description: 'add',
   aliases: [],
@@ -19,43 +18,19 @@ module.exports = {
     const guilddata = await client.db.islandinfo.findOne({ guildid: message.guild.id });
     let target = message.mentions.members.first() || message.guild.members.cache.get(msgArr[1]) || message.mentions.roles.first();
     const amount = parseInt(msgArr[2], 10);
-=======
-  data: new SlashCommandBuilder()
-    .setName('join')
-    .setDescription('Use this when you are ready to join the rest of the server')
-    .addStringOption(option =>
-      option
-        .setName('target')
-        .setDescription('The target user(s) to add currency to')
-        .setRequired(true))
-    .addStringOption(option =>
-      option
-        .setName('amount')
-        .setDescription('The amount of currency to add')
-        .setRequired(true)),
-  async execute(interaction) {
-    const guilddata = await interaction.client.db.config.findOne({
-      id: interaction.guild.id,
-    });
-    if (!guilddata.economy || guilddata.economy === 'true') {
-    let tar = interaction.options.getString('target')
-    let amount = parseInt(interaction.options.getString('amount'), 10);
-    const guilddata = await interaction.client.db.islandinfo.findOne({ guildid: interaction.guild.id });
-    let target = tar.mentions.members.first() || interaction.guild.members.cache.get(tar) || tar.mentions.roles.first();
->>>>>>> Stashed changes
     if (isNaN(amount)) {
-      interaction.reply('Enter a valid amount to add');
+      message.channel.send('Enter a valid amount to add');
       return;
     }
-    if (tar === 'premium') {
+    if (msgArr[1] === 'premium') {
       target = 'premium users'
       let i = 0;
       const user = await client.db.islandinfo.find().toArray()
       async function complete() {
         const embed = new Discord.MessageEmbed()
         .setColor('GREEN')
-        .setDescription(`✅ ${interaction.member} gave ${i} ${target} ${amount} ${guilddata.currencyname ? guilddata.currencyname : 'Bells'}!`);
-        interaction.reply({embeds: [embed]})
+        .setDescription(`✅ ${message.author} gave ${i} ${target} ${amount} ${guilddata.currencyname ? guilddata.currencyname : 'Bells'}!`);
+        message.channel.send({embeds: [embed]})
       }
       function addPremium(amt) {
         user.forEach(check => {
@@ -63,11 +38,7 @@ module.exports = {
           if (check.hasPremium === 'true') {
             i ++;
             console.log(check.id)
-<<<<<<< Updated upstream
            client.db.userdata.updateOne({id: check.id, guildID: message.guild.id}, {$inc: {coins: amount}}, {upsert: true})
-=======
-           interaction.client.db.userdata.updateOne({id: check.id, guildID: interaction.guild.id}, {$inc: {coins: amount}}, {upsert: true})
->>>>>>> Stashed changes
          } } catch(err) {}
         })
       }
@@ -76,27 +47,19 @@ module.exports = {
     }
     const embed = new Discord.MessageEmbed()
     .setColor('#5b4194')
-    .setDescription(`✅ ${interaction.member} gave ${target} ${amount} ${guilddata.currencyname ? guilddata.currencyname : 'Bells'}!`);
-    const isRole = interaction.guild.roles.cache.has(target.id);
+    .setDescription(`✅ ${message.author} gave ${target} ${amount} ${guilddata.currencyname ? guilddata.currencyname : 'Bells'}!`);
+    const isRole = message.guild.roles.cache.has(target.id);
     if (!isRole) {
-<<<<<<< Updated upstream
     await client.db.userdata.updateOne({ id: target.id, guildID: message.guild.id }, { $inc: { coins: amount } }, { upsert: true });
     message.channel.send({embeds: [embed]});
   } else if (isRole) {
     target.members.forEach(async updateuser => {
     await client.db.userdata.updateOne({ id: updateuser.id, guildID: message.guild.id }, { $inc: {coins: amount } }, { upsert : true });
-=======
-    await interaction.client.db.userdata.updateOne({ id: target.id, guildID: interaction.guild.id }, { $inc: { coins: amount } }, { upsert: true });
-    interaction.reply({embeds: [embed]});
-  } else if (isRole) {
-    target.members.forEach(async updateuser => {
-    await interaction.client.db.userdata.updateOne({ id: updateuser.id, guildID: interaction.guild.id }, { $inc: {coins: amount } }, { upsert : true });
->>>>>>> Stashed changes
     });
-    interaction.reply({embeds: [embed]});
+    message.channel.send({embeds: [embed]});
   }
   } else {
-    return interaction.reply('Economy is disabled on this guild!');
+    return message.channel.send('Economy is disabled on this guild!');
   }
   },
 };
