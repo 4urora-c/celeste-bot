86ead3aa392cb8dbaf82f83582d14a4ea52551d3
Eamon <eamon.wong@outlook.com>
2021-10-21 11:47:48 +0800

    Update colour.js

--- commands/economyCommands/colour.js
+++ commands/economyCommands/colour.js
@@ -14,22 +14,34 @@ module.exports = {
     if(interaction.member.roles.cache.some(r => r.name === interaction.member.id)) {
       const role = interaction.guild.roles.cache.find(x => x.name === interaction.member.id);
       await interaction.deferReply()
+      try {
       role.edit({color: interaction.options.getString('hex')}).catch(console.error)
+    } catch(e) {
+      return interaction.reply('This colour does not exist!')
+    }
       interaction.editReply('Your colour role has been edited!')
     } else {
       const role = interaction.guild.roles.cache.find(x => x.name === interaction.member.id);
       if (!role) {
         await interaction.deferReply()
+        try {
         const create = await interaction.guild.roles.create({
           name: interaction.member.id,
           color: interaction.options.getString('hex'),
           position: 76
         }).catch(console.error)
+      } catch(e) {
+        return interaction.reply('This colour does not exist!')
+      }
         interaction.member.roles.add(create)
         interaction.editReply('A colour role has been added to you!')
       } else {
         await interaction.deferReply()
+        try {
         role.edit({color: interaction.options.getString('hex')}).catch(console.error)
+      } catch(e) {
+        return interaction.reply('This colour does not exist!')
+      }
         interaction.member.roles.add(role)
         interaction.editReply('Your colour role has been edited and added to you!')
       }
