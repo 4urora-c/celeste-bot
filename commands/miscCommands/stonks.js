cec9e51c0bb96ecb310637f7151da61e6e1b47fe
Eamon <eamon.wong@outlook.com>
2021-10-23 00:35:37 +0800

    Update stonks.js

--- commands/miscCommands/stonks.js
+++ commands/miscCommands/stonks.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const { getTracks, getPreview } = require("spotify-url-info")
+const { SlashCommandBuilder } = require('@discordjs/builders');
 module.exports = {
   data: new SlashCommandBuilder()
       .setName('stonks')
