234ab1fd06a4edef211dcb261a8d454eaecb9d14
Eamon <eamon.wong@outlook.com>
2021-11-24 23:40:40 +0800

    Update remove.js

--- commands/modCommands/remove.js
+++ commands/modCommands/remove.js
@@ -15,14 +15,13 @@ module.exports = {
         .setDescription('The user whose info you want to reset')
         .setRequired(true)),
   async execute(interaction) {
-    await interaction.deferReply()
     const user = interaction.guild?.members.cache.get(interaction.options.getUser('target').id);
       const userdata = await interaction.client.db.islandinfo.findOne({ id: user.id });
       if (userdata) {
         await interaction.client.db.islandinfo.removeOne({ id: user.id });
-        return await interaction.editReply(`${user.user.tag}'s island info has been removed!`);
+        return interaction.reply(`${user.user.tag}'s island info has been removed!`);
       } else {
-        return await interaction.editReply('Specified user doesn\'t have any island info!');
+        return interaction.reply('Specified user doesn\'t have any island info!');
       }
   },
 };
